1e74a5caf8bb091be95b5bc304ac05ecfbb79e82
shaurya048 <048shaurya.verma@gmail.com>
2026-04-16 11:30:24 +0530

    new changes

---
+++ src/Trie/Basics.java
@@ -0,0 +1,18 @@
+package Trie;
+import java.util.*;
+
+
+public class Basics {
+    static class Node{
+        int data;
+        Node left,right;
+        public Node(int data){
+            this.data=data;
+            left=right=null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
